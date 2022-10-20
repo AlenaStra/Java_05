7eaa98cea9e8f0c7758ecf7365631b5a7c71f6e6
AlenaStra <alena.strakouskaya@gmail.com>
2022-10-19 21:49:56 -0500

    first commit

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+
+//    public String[] fizzBuzz(int start, int end) {
+//        if (start <= end) {
+//            String[] array = new String[end - start + 1];
+//            for(int i = 0; i < array.length && start <= end; i++) {
+//                if(start % 3 == 0) {
+//                    array
+//                }
+//
+//            }
+//        }
+//
+//        return new String[0];
+//
+//    }
+}
